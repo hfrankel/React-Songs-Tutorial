11ddc95bc332583a3e8578c321abd4da2467c943
Harry Frankel <hfranks92@gmail.com>
2020-01-14 22:16:38 +1100

    Tutorial

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -11,4 +11,8 @@ class SongList extends Component {
     }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
